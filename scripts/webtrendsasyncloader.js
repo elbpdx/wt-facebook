e997f6d07c1086a3f7492c05cd3ac8e3961fed2a
Eric Butler <elbpdx@gmail.com>
2011-02-06 23:13:30 -0800

    added goodies for tracking the like buttons and simple webtrends tracking

---
+++ scripts/webtrendsasyncloader.js
@@ -0,0 +1,13 @@
+// WebTrends SmartSource Data Collector Async Tag
+// Version: 9.4.2
+
+var dcsQ=[{cmd:"collect"}];
+(function(){
+	var s,s2;
+	s=document.createElement("script");
+	s.type="text/javascript";
+	s.async=true;
+	s.src="/scripts/webtrendsasync.js";
+	s2=document.getElementsByTagName("script")[0];
+	s2.parentNode.insertBefore(s,s2);
+})();
